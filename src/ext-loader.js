5017b14986e36b9155288da60e100e2759ae9007
unknown <394229785@qq.com>
2016-05-19 14:43:00 +0800

    跨主题！所有主题都能使用~

--- src/ext-loader.js
+++ src/ext-loader.js
@@ -1,14 +1,14 @@
 var extStyle = `
 <style>
-.vscode-theme-defaults-themes-dark_plus-json>.overflow-guard>.vscode-theme-defaults-themes-dark_plus-json.vs-dark>.monaco-editor-background{background: none;}
+[id='workbench.parts.editor']>.content>.one-editor-container .monaco-editor>.overflow-guard>.monaco-scrollable-element>.monaco-editor-background{background: none;}
 
-.monaco-editor-background>.content>.one-editor-container:nth-of-type(1) .vscode-theme-defaults-themes-dark_plus-json>.overflow-guard>.vscode-theme-defaults-themes-dark_plus-json.vs-dark{background-image: url(http://i3.buimg.com/9d523a20ea864bc2.png);}
+[id='workbench.parts.editor']>.content>.one-editor-container:nth-child(1) .monaco-editor>.overflow-guard>.monaco-scrollable-element{background-image: url(http://i3.buimg.com/9d523a20ea864bc2.png);}
 
-.monaco-editor-background>.content>.one-editor-container:nth-of-type(3) .vscode-theme-defaults-themes-dark_plus-json>.overflow-guard>.vscode-theme-defaults-themes-dark_plus-json.vs-dark{background-image: url(http://i3.buimg.com/1dd6b5d20c334350.png);}
+[id='workbench.parts.editor']>.content>.one-editor-container:nth-child(3) .monaco-editor>.overflow-guard>.monaco-scrollable-element{background-image: url(http://i3.buimg.com/1dd6b5d20c334350.png);}
 
-.monaco-editor-background>.content>.one-editor-container:nth-of-type(5) .vscode-theme-defaults-themes-dark_plus-json>.overflow-guard>.vscode-theme-defaults-themes-dark_plus-json.vs-dark{background-image: url(http://i3.buimg.com/0ce7df680df89325.png);}
+[id='workbench.parts.editor']>.content>.one-editor-container:nth-child(5) .monaco-editor>.overflow-guard>.monaco-scrollable-element{background-image: url(http://i3.buimg.com/0ce7df680df89325.png);}
 
-.monaco-editor-background>.content>.one-editor-container .vscode-theme-defaults-themes-dark_plus-json>.overflow-guard>.vscode-theme-defaults-themes-dark_plus-json.vs-dark{background-position:100% 100%;background-repeat:no-repeat;}
+[id='workbench.parts.editor']>.content>.one-editor-container .monaco-editor>.overflow-guard>.monaco-scrollable-element{background-position:100% 100%;background-repeat:no-repeat;}
 </style>
 `;
 var extDiv = document.createElement("div");
