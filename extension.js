82f39ada76903166e42bad8e9746c3be7a3cff0e
谢爽 <394229785@qq.com>
2016-08-27 16:44:19 +0800

    remove tip in console

--- extension.js
+++ extension.js
@@ -9,7 +9,7 @@ function activate(context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "background" is now active!');
+    // console.log('Congratulations, your extension "background" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
