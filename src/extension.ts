81f5bce993743830ab2a84d7508aa01c752c2a70
shalldie <394229785@qq.com>
2017-10-30 00:31:36 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import background from './background';
+import vsHelp from './vsHelp';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -15,11 +16,8 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello Worldaaaaaa!');
+    let disposable = vscode.commands.registerCommand('extension.background.info', function () {
+        vsHelp.showInfo('You can config your background in settings.json. Enjoy it!');
     });
 
     context.subscriptions.push(disposable);
