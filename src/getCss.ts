0f8da0c55b10e71bc98e9eada8da4d6e016a60bc
using <using.lei@7road.com>
2018-04-29 23:42:26 +0800

    为每张背景图片配置单独样式 #43

--- src/getCss.ts
+++ src/getCss.ts
@@ -6,44 +6,94 @@ import version from './version';
  * 
  * @export
  * @param {Array<string>} arr 图片数组
- * @param {any} [style={}] 自定义样式
+ * @param {any} [style={}|{}[]] 自定义样式
  * @param {boolean} [useFront=true] 是否用前景图
  * @returns 
  */
-export default function (arr: Array<string>, style = {}, useFront = true) {
+export default function (arr: Array<string>, style: {} | {}[], useFront = true) {
     let [img0, img1, img2] = (arr && arr.length) ?
         [encodeURI(arr[0] || 'none'),
         encodeURI(arr[1] || 'none'),
         encodeURI(arr[2] || 'none')] : defBase64;
 
-    let styleArr: string[] = [];
+    let styleArr1: string[] = [];
+    let styleArr2: string[] = [];
+    let styleArr3: string[] = [];
 
-    for (let k in style) {
-        // 在使用背景图时，排除掉 pointer-events
-        if (!useFront && ~['pointer-events', 'z-index'].indexOf(k)) {
-            continue;
-        }
+    let isArrayStyle: boolean = Array.isArray(style);
+    if (isArrayStyle) {
+        let arrLen = (<{}[]>style).length;        
+        if (arrLen > 0) {            
+            let inx: number = 0;
+            for (let k in style[inx]) {
+                // 在使用背景图时，排除掉 pointer-events
+                if (!useFront && ~['pointer-events', 'z-index'].indexOf(k)) {
+                    continue;
+                }
+        
+                if (style[inx].hasOwnProperty(k)) {
+                    styleArr1.push(`${k}:${style[inx][k]}`);
+                }
+            }
 
-        if (style.hasOwnProperty(k)) {
-            styleArr.push(`${k}:${style[k]}`);
+            inx++;
+            inx = Math.min(arrLen - 1, inx);
+            for (let k in style[inx]) {
+                // 在使用背景图时，排除掉 pointer-events
+                if (!useFront && ~['pointer-events', 'z-index'].indexOf(k)) {
+                    continue;
+                }
+        
+                if (style[inx].hasOwnProperty(k)) {
+                    styleArr2.push(`${k}:${style[inx][k]}`);
+                }
+            }
+
+            inx++;
+            inx = Math.min(arrLen - 1, inx);
+            for (let k in style[inx]) {
+                // 在使用背景图时，排除掉 pointer-events
+                if (!useFront && ~['pointer-events', 'z-index'].indexOf(k)) {
+                    continue;
+                }
+        
+                if (style[inx].hasOwnProperty(k)) {
+                    styleArr3.push(`${k}:${style[inx][k]}`);
+                }
+            }
+        }        
+    } else {
+        for (let k in style) {
+            // 在使用背景图时，排除掉 pointer-events
+            if (!useFront && ~['pointer-events', 'z-index'].indexOf(k)) {
+                continue;
+            }
+    
+            if (style.hasOwnProperty(k)) {
+                styleArr1.push(`${k}:${style[k]}`);
+            }
         }
+        styleArr2 = styleArr1;
+        styleArr3 = styleArr1;
     }
 
     // 在前景图时使用 ::after
     let frontContent = useFront ? '::after' : '::before';
 
     // 样式内容
-    let styleContent = styleArr.join(';') + ';';
+    let styleContent1 = styleArr1.join(';') + ';';
+    let styleContent2 = styleArr2.join(';') + ';';
+    let styleContent3 = styleArr3.join(';') + ';';
 
     let content = `
     
 /*css-background-start*/
 /*background.ver.${version}*/
-.editor-one>.container>.editor-container>.monaco-editor>.overflow-guard>.monaco-scrollable-element:nth-child(2)${frontContent}{background-image: url('${img0}');${styleContent}}
+.editor-one>.container>.editor-container>.monaco-editor>.overflow-guard>.monaco-scrollable-element:nth-child(2)${frontContent}{background-image: url('${img0}');${styleContent1}}
 
-.editor-two>.container>.editor-container>.monaco-editor>.overflow-guard>.monaco-scrollable-element:nth-child(2)${frontContent}{background-image: url('${img1}');${styleContent}}
+.editor-two>.container>.editor-container>.monaco-editor>.overflow-guard>.monaco-scrollable-element:nth-child(2)${frontContent}{background-image: url('${img1}');${styleContent2}}
 
-.editor-three>.container>.editor-container>.monaco-editor>.overflow-guard>.monaco-scrollable-element:nth-child(2)${frontContent}{background-image: url('${img2}');${styleContent}}
+.editor-three>.container>.editor-container>.monaco-editor>.overflow-guard>.monaco-scrollable-element:nth-child(2)${frontContent}{background-image: url('${img2}');${styleContent3}}
 
 [id='workbench.parts.editor']>.content>.one-editor-silo .monaco-editor>.overflow-guard>.monaco-scrollable-element>.monaco-editor-background{background: none;}
 
