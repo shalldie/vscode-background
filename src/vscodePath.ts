51be9549369a85ea421550972a22cfe2d995c72b
Kuresaru <1142305747@qq.com>
2019-12-13 17:23:46 +0800

    add feature: apply css with sudo - 自动提权写入css文件

--- src/vscodePath.ts
+++ src/vscodePath.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
+import { execPath } from 'process'
 
 // 基础目录
 const base = path.dirname(require.main.filename);
@@ -10,6 +11,34 @@ const cssPath = path.join(base, 'vs', 'workbench', cssName);
 
 // electron 入口文件所在文件夹
 const indexDir = path.join(base, 'vs', 'workbench', 'electron-browser', 'bootstrap');
+
+function getCLIPath(): string {
+    // TODO: 这两个变量不知道取的对不对
+    let isBuilt = !process.env['VSCODE_DEV'];
+    let appRoot = base;
+    // TODO: 实在不知道怎么取了。。直接默认值
+    let applicationName = 'code';
+    // 下边代码来自vscode源码/src/vs/platform/environment/node/environmentService.ts line:48
+    // Windows
+    if (process.platform == 'win32') {
+        if (isBuilt) {
+            return path.join(path.dirname(execPath), 'bin', `${applicationName}.cmd`);
+        }
+        return path.join(appRoot, 'scripts', 'code-cli.bat');
+    }
+    // Linux
+    if (process.platform == 'linux' || process.platform == 'android') {
+        if (isBuilt) {
+            return path.join(path.dirname(execPath), 'bin', `${applicationName}`);
+        }
+        return path.join(appRoot, 'scripts', 'code-cli.sh');
+    }
+    // macOS
+    if (isBuilt) {
+        return path.join(appRoot, 'bin', 'code');
+    }
+    return path.join(appRoot, 'scripts', 'code-cli.sh');
+}
 
 export default {
     /**
@@ -23,5 +52,6 @@ export default {
     /**
      * electron 入口文件所在文件夹
      */
-    indexDir
+    indexDir,
+    getCLIPath
 };
