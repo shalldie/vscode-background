708f7472295f0030e17ec701f0755c9d6082a807
NoDocCat <nodoccat@outlook.com>
2020-05-10 16:26:39 +0800

    实现背景图片循环使用

--- src/getCss.ts
+++ src/getCss.ts
@@ -25,6 +25,21 @@ function getStyleByOptions(options: object, useFront: boolean): string {
 }
 
 /**
+ * 生成样式项
+ * @param n             nth-child的参数
+ * @param url           图片url
+ * @param useFront      是否使用前景图
+ * @param otherStyle    追加的额外样式
+ */
+function genStyleItem(n: string, url: string, useFront: boolean, otherStyle: string): string {
+    const frontContent = useFront ? '::after' : '::before';
+    let selector = `[id="workbench.parts.editor"] .split-view-view:nth-child(${n}) .editor-container `;
+    selector += `.editor-instance>.monaco-editor .overflow-guard>.monaco-scrollable-element${frontContent}`;
+
+    return `${selector} { background-image: url('${url}'); ${otherStyle} }`;
+}
+
+/**
  * 生成css样式
  *
  * @export
@@ -32,42 +47,48 @@ function getStyleByOptions(options: object, useFront: boolean): string {
  * @param {any} [style={}] 自定义样式
  * @param {Array<any>} [styles=[]] 每个背景图的自定义样式
  * @param {boolean} [useFront=true] 是否用前景图
+ * @param {boolean} [loop=false] 是否循环使用图片
  * @returns
  */
-export default function (arr: Array<string>, style: any = {}, styles: Array<any> = [], useFront = true): string {
-    const [img0, img1, img2] = (arr && arr.length)
-        ? [
-            arr[0] || 'none',
-            arr[1] || 'none',
-            arr[2] || 'none'
-        ]
-        : defBase64;
-
+export default function (
+    arr: Array<string>,
+    style: any = {},
+    styles: Array<any> = [],
+    useFront = true,
+    loop: boolean = false
+): string {
     const defStyle = getStyleByOptions(style, useFront); // 默认样式
-    const [styel0, style1, style2] = [                   // 3个子项样式
-        defStyle + getStyleByOptions(styles[0], useFront),
-        defStyle + getStyleByOptions(styles[1], useFront),
-        defStyle + getStyleByOptions(styles[2], useFront)
-    ];
+    let images = (arr && arr.length) ? arr : defBase64;
+    let content = '\n/*css-background-start*/\n';
 
-    // 在前景图时使用 ::after
-    const frontContent = useFront ? '::after' : '::before';
+    // 追加版本号
+    content += `/*${BACKGROUND_VER}.${version}*/\n`;
 
-    const content = `
+    if (!loop) {
+        // 非循环使用生成nth-child(index + 1)
+        images.forEach((item, index) => {
+            const n = `${index + 1}`
+            const otherStyle = defStyle + getStyleByOptions(styles[index], useFront)
+            content += genStyleItem(n, item, useFront, otherStyle) + '\n'
+        })
+    } else {
+        // 循环使用逆序循环生成nth-child(length * n - index)
+        images = images.reverse()
+        const styleList = styles.reverse()
+        const length = images.length
 
-/*css-background-start*/
-/*${BACKGROUND_VER}.${version}*/
+        images.forEach((item, index) => {
+            const n = `${length}n - ${index}`
+            const otherStyle = defStyle + getStyleByOptions(styleList[index], useFront)
+            content += genStyleItem(n, item, useFront, otherStyle) + '\n'
+        })
+    }
 
-[id="workbench.parts.editor"] .split-view-view:nth-child(1) .editor-container .editor-instance>.monaco-editor .overflow-guard>.monaco-scrollable-element${frontContent}{background-image: url('${img0}');${styel0}}
+    // 追加额外样式
+    content += '[id="workbench.parts.editor"] .split-view-view .editor-container .editor-instance>.monaco-editor .overflow-guard>.monaco-scrollable-element>.monaco-editor-background{background: none;}\n';
 
-[id="workbench.parts.editor"] .split-view-view:nth-child(2) .editor-container .editor-instance>.monaco-editor .overflow-guard>.monaco-scrollable-element${frontContent}{background-image: url('${img1}');${style1}}
-
-[id="workbench.parts.editor"] .split-view-view:nth-child(3) .editor-container .editor-instance>.monaco-editor .overflow-guard>.monaco-scrollable-element${frontContent}{background-image: url('${img2}');${style2}}
-
-[id="workbench.parts.editor"] .split-view-view .editor-container .editor-instance>.monaco-editor .overflow-guard>.monaco-scrollable-element>.monaco-editor-background{background: none;}
-
-/*css-background-end*/
-`;
+    // 追加结束标志
+    content += '/*css-background-end*/\n';
 
     return content;
 }
