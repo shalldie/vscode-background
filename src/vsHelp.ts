9a47348e5e71b6ad9d9915ecb60fead13b957490
舰队的偶像-岛风酱! <frg2089@outlook.com>
2022-06-08 18:57:19 +0800

    检查是否有安装 lehni.vscode-fix-checksums 如果有 就询问用户是否要使用这个扩展

--- src/vsHelp.ts
+++ src/vsHelp.ts
@@ -17,7 +17,20 @@ export const vsHelp = {
      * @param {string} content 提示内容
      * @returns {Thenable<void>}
      */
-    showInfoRestart(content: string): Thenable<void> {
+    async showInfoRestart(content: string): Promise<void> {
+        const fixChecksums = vscode.extensions.getExtension('lehni.vscode-fix-checksums');
+        if (fixChecksums) {
+            const result = await vscode.window.showInformationMessage(
+                "We found the 'Fix VSCode Checksums' Extension has been installed. " +
+                    'Do you want to use it to remove [unsupported] tag? ',
+                'Yes',
+                'No'
+            );
+            if (result === 'Yes') {
+                vscode.commands.executeCommand('fixChecksums.apply');
+                return;
+            }
+        }
         return vscode.window.showInformationMessage(content, { title: 'Restart vscode' }).then(function (item) {
             if (!item) return;
             vscode.commands.executeCommand('workbench.action.reloadWindow');
