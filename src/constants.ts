190f85ebd4ce0825d4a166d70dfde46f3464ded2
frg2089 <frg2089@outlook.com>
2022-07-19 17:25:58 +0800

    使扩展的发布者被修改后，卸载命令正常工作

--- src/constants.ts
+++ src/constants.ts
@@ -8,3 +8,12 @@ export const BACKGROUND_VER = 'background.ver';
 
 /** 文件编码 */
 export const ENCODE = 'utf-8';
+
+/** 发布者 */
+export const PUBLISHER: string = pkg.publisher;
+
+/** 扩展名 */
+export const EXTENSION_NAME: string = pkg.name;
+
+/** 扩展ID */
+export const EXTENSION_ID = `${PUBLISHER}.${EXTENSION_NAME}`;
