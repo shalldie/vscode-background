76cfd7dbf2b8c8839034637705049a0aad5f7115
NoDocCat <nodoccat@outlook.com>
2022-11-10 15:43:26 +0800

    feat: 提供内联 css 支持

--- src/background/CssGenerator/CssGenerator.base.ts
+++ src/background/CssGenerator/CssGenerator.base.ts
@@ -1,7 +1,18 @@
 import fs from 'fs';
 import path from 'path';
 import { URL } from 'url';
+import { compile, serialize, stringify } from 'stylis';
 import { VERSION, BACKGROUND_VER } from '../../constants';
+
+/**
+ * 用于触发开发工具 css in js 语言支持
+ */
+export function css(template: TemplateStringsArray, ...args: any[]) {
+    return template.reduce((prev, curr, i) => {
+        const arg = Array.isArray(args[i]) ? args[i].join('') : args[i];
+        return prev + curr + (arg ?? '');
+    }, '');
+}
 
 export abstract class AbsCssGenerator<T = any> {
     /**
@@ -22,19 +33,14 @@ export abstract class AbsCssGenerator<T = any> {
 
     /**
      * 图片预处理
-     *
+     * 在 v1.51.1 版本之后, vscode 将工作区放入 sandbox 中运行并添加了 file 协议的访问限制, 导致使用 file 协议的背景图片无法显示
+     * 当检测到配置文件使用 file 协议时, 需要将图片读取并转为 base64, 而后再插入到 css 中
      * @protected
      * @param {string[]} images 图片列表
      * @return {*}
      * @memberof AbsCssGenerator
      */
     protected async normalizeImages(images: string[]) {
-        /*
-          图片预处理
-          在 v1.51.1 版本之后, vscode 将工作区放入 sandbox 中运行并添加了 file 协议的访问限制, 导致使用 file 协议的背景图片无法显示
-          当检测到配置文件使用 file 协议时, 需要将图片读取并转为 base64, 而后再插入到 css 中
-        */
-
         const list: string[] = []; // 处理后的图片列表
 
         for (const url of images) {
@@ -43,6 +49,13 @@ export abstract class AbsCssGenerator<T = any> {
         }
 
         return list;
+    }
+
+    /**
+     * 编译 css
+     */
+    protected compileCSS(source: string) {
+        return serialize(compile(source), stringify);
     }
 
     protected abstract getCss(options: T): Promise<string>;
